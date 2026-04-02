a9088a5816e5df438c74d5d37867de7563b8f5c9
Kiran Koch-Mathews <kiran.kochmathews@gmail.com>
2026-04-01 23:13:15 -0400

    Week 1 Serial Sparse GPU solver
    CUDA solver uses CSR formatted matrices. I'm assuming that we use a COO-formatted matrix as the input.
    
    I also made a simple test in empty.cc

--- include/MAKAsolve/Solver.h
+++ include/MAKAsolve/Solver.h
@@ -1,5 +1,23 @@
+#pragma once
+
 namespace maka {
 
-class Solver {};
+// Solves a sparse linear system Ax = b on the GPU
+class Solver {
+ public:
+  Solver(int numRows, int numNonZeros, const int* rowIndices, const int* colIndices, const double* values);
+  ~Solver();
+  
+  void solve(const double* rhs, double* solution);
+
+ private:
+  int _numRows;
+  int _numNonZeros;
+
+  // CSR arrays on GPU
+  int* _rowOffsets;
+  int* _colIndices;
+  double* _values;
+};
 
 } // namespace maka
--- test/empty.cc
+++ test/empty.cc
@@ -1,5 +1,26 @@
 #include <MAKAsolve/Solver.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main() {
-	maka::Solver s;
+  int numRows = 3;
+  int numNonZeros = 3;
+
+  // COO format
+  int rowIndices[] = {0, 1, 2};
+  int colIndices[] = {0, 1, 2};
+  double values[] = {4.0, 3.0, 2.0};
+
+  double rhs[] = {8.0, 6.0, 4.0};
+  double solution[] = {0.0, 0.0, 0.0};
+
+  maka::Solver s(numRows, numNonZeros, rowIndices, colIndices, values);
+  s.solve(rhs, solution);
+
+  cout << "Solution = {" << solution[0] << ", " << solution[1] << ", " << solution[2] << "}\n";
+	cout << "Expected = {2, 2, 2}\n";
+
+  return 0;
 }
